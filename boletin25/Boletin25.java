9f823cfdfb550bee2b26bee7961b1ca5577e1d3e
socamporomani <43774853+socamporomani@users.noreply.github.com>
2019-04-01 12:33:40 +0200

    acabado

---
+++ boletin25/Boletin25.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin25;
+
+/**
+ *
+ * @author socamporomani
+ */
+public class Boletin25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+uno a =new uno();
+a.setVisible(true);
+
+    }
+    
+}
